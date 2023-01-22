2c1945f5273f3fc063c41fd7dfd23e699052d8ba
Camilo Jimenez <Camiloj896@gmail.com>
2023-01-22 13:45:35 -0500

    :hammer: feat: add inicial config to create a react project

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './index.scss';
+
+function App() {
+  return <h2>Hola</h2>
+}
+
+ {/* <Route exact path="/" element={<Home />} /> */}
+        {/* <Route exact path="/" element={<Home />} />
+        <Route exact path="/checkout" element={<Checkout />} />
+        <Route exact path="/checkout/information" element={<Information />} />
+        <Route exact path="/checkout/payment" element={<Payment />} />
+        <Route exact path="/checkout/success" element={<Success />} /> */}
+
+export default App;
